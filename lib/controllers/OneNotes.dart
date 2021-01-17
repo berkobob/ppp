4a8b43e84770249945a5af1f047ac064b4712a48
Antoine Lever <alever@omniis.com>
2021-01-17 17:50:45 +0000

    Scope

--- lib/controllers/OneNotes.dart
+++ lib/controllers/OneNotes.dart
@@ -1,3 +1,4 @@
+import 'dart:developer' as d;
 import 'package:ppp/models/item.dart';
 import 'package:ppp/services/microsoft.dart';
 
@@ -10,23 +11,39 @@ class OneNotes extends Base {
     updateStatus(Service.busy);
     final result = await microsoft.isConnected();
     updateStatus(result != null ? Service.access : Service.denied);
-    getItems();
+    await getItems();
   }
 
   Future getItems() async {
     updateStatus(Service.busy);
-    final sections = await microsoft.getSections();
-    sections.forEach((section) async {
+    final allSections = await microsoft.getSections();
+    final sections =
+        allSections.where((section) => section.notebook == 'RPM').toList();
+    var count = 1;
+    await Future.forEach(sections, (section) async {
       final pages = await microsoft.getPages(section);
-      pages.forEach((page) async {
-        List<Item> i = await microsoft.getContent(page);
-        if (i.length > 0) {
-          items = items + i;
+      d.log('Reading section ${count++} of ${sections.length}');
+      await Future.forEach(pages, (page) async {
+        final newItems = await microsoft.getContent(page);
+        if (newItems.length > 0) {
+          items += newItems;
+          notifyListeners();
         }
       });
     });
     updateStatus(Service.access);
   }
+
+  // Future getItems() async {
+  //   updateStatus(Service.busy);
+
+  //   final sections = await microsoft.getSections();
+  //   final pages =
+  //       Future.wait(sections.map((section) => microsoft.getPages(section)));
+  //   pages.((page) => microsoft.getContent(page));
+  //   print(x);
+  //   updateStatus(Service.access);
+  // }
 
   Future getItemsOld() async {
     if (status != Service.access) return;
--- lib/models/section.dart
+++ lib/models/section.dart
@@ -2,15 +2,17 @@ class Section {
   String id;
   String displayName;
   String pagesUrl;
+  String notebook;
 
   Section(Map<String, dynamic> json) {
     id = json['id'];
     displayName = json['displayName'];
     pagesUrl = json['pagesUrl'];
+    notebook = json['parentNotebook']['displayName'];
   }
 
   @override
   String toString() {
-    return displayName;
+    return '$notebook in $displayName';
   }
 }
